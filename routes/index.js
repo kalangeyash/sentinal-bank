ed7d74ed3fba4b048082b3a0f377996cd9a05017
kalangeyash <yashkalange127@gmail.com>
2024-07-11 11:11:39 +0530

    implemented transactions

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 const router = express.Router()
 import userRouter from './user.js'
+import accountRouter from './account.js'
 
 router.use('/user', userRouter)
+router.use('/account', accountRouter)
 
 export default router
